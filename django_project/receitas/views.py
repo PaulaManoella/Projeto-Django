0197f41ab6854220f55467dc3529503ab38cd151
mnoellap <pmnoella1@gmail.com>
2025-08-20 21:44:17 -0300

    passando context como parametro em render() para capturar dados de forma dinamica na pagina html

--- django_project/receitas/views.py
+++ django_project/receitas/views.py
@@ -3,9 +3,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    
+    receitas = {
+        1: 'lasanha',
+        2: 'strogonoff',
+        3: 'frango a milanesa',
+        4: 'feijoada'   
+    }
+    
+    dados_receitas={
+        'nomes_receitas': receitas
+    }
+    
     # usando render para renderizar o arquivo html
     return render(request,
-                  'index.html')
+                  'index.html',
+                #   passando context como parametro para renderizar na pagina html
+                  dados_receitas)
 
 def receita(request):
     return render(request,
